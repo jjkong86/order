ed4c90cf15b366abfb885598131a41859b52c318
정재공 <jjkong86@gmail.com>
2020-01-31 17:35:43 +0900

    future 제거, Atomic 활용

--- src/order/net/class101/server1/testCase/MultiThreadStockTest.java
+++ src/order/net/class101/server1/testCase/MultiThreadStockTest.java
@@ -1,9 +1,11 @@
 package order.net.class101.server1.testCase;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Test;
 
@@ -16,23 +18,30 @@ import order.net.class101.server1.util.Utils;
 public class MultiThreadStockTest {
 
 	@Test(expected = SoldOutException.class)
-	public void 주문_완료_재고_검증() throws InterruptedException {
+	public void 주문_완료_재고_검증() throws InterruptedException, ExecutionException {
 		int num = 11;
 		ExecutorService e = Executors.newFixedThreadPool(num);
 		Map<String, Goods> map = Utils.buildGoodsList();
 		String goodsId = "91008";
 		Order orderList = new Order();
 		orderList.getGoods().add(Utils.getNewInstance(map.get(goodsId), 1));
-		try {
-			for (int i = 0; i < num; i++) {
-				Future<?> f = e.submit(() -> {
+		AtomicBoolean success = new AtomicBoolean(true);
+		for (int i = 0; i < num; i++) {
+			e.execute(() -> {
+				try {
 					new OrderServiceImpl().chkOrderAmount(map, orderList);
-				});
-				f.get();
-			}
-		} catch (Exception e1) {
-			e1.printStackTrace();
-			throw new SoldOutException(e1.getMessage());
+				} catch (Exception e1) {
+					e1.printStackTrace();
+					success.set(false);
+				}
+			});
 		}
+
+		e.awaitTermination(3, TimeUnit.SECONDS);
+
+		if (!success.get()) {
+			throw new SoldOutException("실패");
+		}
+
 	}
 }
